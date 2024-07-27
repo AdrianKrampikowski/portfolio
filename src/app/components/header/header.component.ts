ed432482cecedb3a75fb361a75b850618cf12193
AdrianKrampikowski <info@adrian-krampikowski.com>
2024-07-27 10:37:05 +0200

    change

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -35,3 +35,89 @@ export class HeaderComponent {
     this.displayMenuItem.emit(this.showMenuItems);
   }
 }
+
+// const ProductsModel = require("../models/product")
+
+// const getAllProducts = async (req, resp) => {
+//     const { price } = req.query
+//     const queryObject = {}
+//     if (price) {
+//         queryObject.price = price
+//     }
+//     let data = await ProductsModel.find(queryObject)
+//     resp.status(200).json(data)
+// }
+
+// const getallProductTesting = async (req, resp) => {
+//     let { sort } = req.query
+//     const queryObject = req.query
+//     let apiData
+//     if (sort) {
+//         let sortFix = sort.replaceAll(",", " ")
+//         sort = sortFix
+//         delete queryObject.sort
+//         apiData = apiData.sort(sort)
+//     }
+//     apiData = ProductsModel.find(queryObject)
+//     let data = await apiData
+//     resp.status(200).json(data)
+// }
+
+// const getSelectedProducts = async (req, resp) => {
+//     let { select } = req.query
+//     let queryObject = req.query
+//     let apiData
+//     if (select) {
+//         let changedSelect = select.replaceAll(",", " ")
+//         select = changedSelect
+//         delete queryObject.select
+
+//     }
+
+//     apiData = ProductsModel.find(queryObject)
+//     let data = await apiData.select(select)
+
+//     resp.status(200).json(data)
+// }
+
+// const selectSortProducts = async (req, resp) => {
+//     let { select, sort } = req.query
+//     let queryObject = req.query
+//     let apiData
+//     if (select) {
+//         let changeSelect = select.replaceAll(",", " ")
+//         select = changeSelect
+//         delete queryObject.select
+//     }
+//     if (sort) {
+//         let changeSort = sort.replaceAll(",", " ")
+//         sort = changeSort
+//         delete queryObject.sort
+//     }
+//     apiData = ProductsModel.find(queryObject)
+//     let selectedSortedData = apiData.sort(sort).select(select)
+//     let data = await selectedSortedData
+//     resp.status(200).json(data)
+// }
+
+// const getAllProductsWithPagination = async (req, resp) => {
+//     let page = Number(req.query.page) || 1
+//     let limit = Number(req.query.limit) || 5
+//     let skip = (page - 1) * limit
+//     let queryObject = req.query
+//     if (page) {
+//         delete queryObject.page
+//     }
+//     if (limit) {
+//         delete queryObject.limit
+//     }
+//     let apiData = ProductsModel.find({})
+//     apiData = apiData.skip(skip).limit(limit)
+//     let data = await apiData
+
+//     // let data = await ProductsModel.find(queryObject)
+//     resp.status(200).json({ data: data, pageNumber: page, limit: limit })
+// }
+
+// module.exports = { getAllProducts, getallProductTesting, getSelectedProducts, selectSortProducts, getAllProductsWithPagination }
+
